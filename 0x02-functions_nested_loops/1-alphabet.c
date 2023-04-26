c6abb953f5c278867ea20f2c5898870baf74fd72
Damilola <dyadegbola@outlook.com>
2023-04-25 17:43:45 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,20 @@
+
 #include "main.h"
 
+
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+
+	void print_alphabet(void)
 {
+		char letter;
 
-print_alphabet(void)
 
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
 }
