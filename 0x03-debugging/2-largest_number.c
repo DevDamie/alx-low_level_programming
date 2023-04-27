c251272403a957cfd4609f2c74f497ac57149925
Damilola <dyadegbola@outlook.com>
2023-04-26 20:13:29 -0700

    largest

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    srand(time(0));
+    a = rand(); b = rand(); c = rand ();
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
