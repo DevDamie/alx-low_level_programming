18915729565536c2302ccb95c05af043061d1092
Damilola <dyadegbola@outlook.com>
2023-04-29 13:45:39 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
-	/**
-	 * main - Entry point
-	 * print _putchar
-	 * Return: 0
-	 */
+
+/**
+ * main - Entry point
+ * Description: prints _putchar
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
