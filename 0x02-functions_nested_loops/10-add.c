1242717104eb55ff23fdabd48a9f4ec620f1576d
Damilola <dyadegbola@outlook.com>
2023-05-01 12:51:50 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @n1: first integer to be added
+ * @n2: second integer to be added
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+
+}
