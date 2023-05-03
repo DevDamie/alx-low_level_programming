5b20712da1eba9ea4e705e2471b40d7038c9fffb
Damilola <dyadegbola@outlook.com>
2023-05-02 18:21:39 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string
+ * starting with the first character
  * @str: the used string reference
  * Return: 0
  */
