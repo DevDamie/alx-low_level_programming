f79e2bbbcee44eb9103c4d8a6652348137c7011c
Damilola <dyadegbola@outlook.com>
2023-05-07 15:45:42 -0700

    This program converts a date to the day of year and determines how many days are left in the year, taking leap year into consideration

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -5,6 +5,13 @@
 * for leap year
 * @month: month in number format
 * @day: day of month
+* Return: day of yea#include "main.h"
+
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
 * Return: day of year
 */
 
