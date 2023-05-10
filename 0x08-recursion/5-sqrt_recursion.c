67bffba7b5a7662faefac80a128facab2a592a76
Damilola <dyadegbola@outlook.com>
2023-05-10 11:38:45 -0700

    5. Your subconscious is looking for the dreamer. Write a function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *_sqrt_recursion - function that returns the natural square root of a number.
+ * @n: integer
+ * @sqr - the natural square root of a number
+ * Return: sqr of  @n if n does not have a natural square root,
+ * the function should return -1
+ */
+
 int _sqr(int sqr, int n)
 {
 	/* int sqr = n / 2; */
@@ -15,12 +23,7 @@ int _sqr(int sqr, int n)
 	return (_sqr(sqr + 1, n));
 
 }
-/**
- * _sqrt_recursion - function that returns the natural square root of a number.
- * @n: integer.
- * Return: sqr of  @n if n does not have a natural square root,
- * the function should return -1
- */
+
 
 int _sqrt_recursion(int n)
 {
@@ -28,16 +31,16 @@ int _sqrt_recursion(int n)
 		return (-1);
 	return (_sqr(1, n));
 
-	/* if(j*j >= n) */
+	/* if (j * j >= n) */
 	/* { */
 	/*	return (_sqrt_recursion(j)); */
 	/* } */
 	/*	return (_sqrt_recursion(j)); */
 	/* int start = 0, end = n; */
 	/* int mid = (start + end)/2; */
-	/* if(mid*mid < n) */
+	/* if (mid * mid < n) */
 	/*	start = mid; */
-	/* if(mid * mid >= n) */
+	/* if (mid * mid >= n) */
 	/*	end = mid; */
 	/* return (mid); */
 }
