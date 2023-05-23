5568fe4cba0e8aab9931863a5c76914746a2f661
Damilola <dyadegbola@outlook.com>
2023-05-23 03:42:34 -0700

    0. Poppy. Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,25 +3,24 @@
 
 /**
  * struct dog - dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * @name: First member -  name of the dog
+ * @age: Second member -  age of the dog
+ * @owner: Third member - name of the owner of the dog
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
+} dog_t; /* dog_t - typedef for struct dog */
 
 
-/**
- * dog_t - typedef for struct dog
- */
+/* Other necessary prototypes */
 
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
