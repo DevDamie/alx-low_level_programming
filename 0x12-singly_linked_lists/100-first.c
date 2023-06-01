892cfa2203645c802acc492920a4d7bfa4c7e793
Damilola <dyadegbola@outlook.com>
2023-06-01 15:28:26 -0700

    5. The Hare and the Tortoise. Write a function that prints a sentence before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,13 +2,11 @@
 #include "lists.h"
 
 /**
- * print_first - prints a sentence before the main
+ * first - a function that prints a sentence before the main
  * function is executed.
- *
- * Return: void.
  */
 
-void print_first(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
