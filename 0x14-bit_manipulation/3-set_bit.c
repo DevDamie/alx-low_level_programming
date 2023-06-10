14e61cc0994d61e2a0a38e786b1683833a7d040f
Damilola <dyadegbola@outlook.com>
2023-06-10 06:37:50 -0700

    3.11. Write a function that sets the value of a bit to 1 at a given index.

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,7 +3,7 @@
 /**
  * set_bit -a function that sets the value of a bit to 1 at a given index.
  * @n: pointer to the number to change
- * @index: index of the bit to set to 1
+ * @index: index (starting from 0) of the bit to set to 1
  *
  * Return: 1 for success, -1 for failure
  */
